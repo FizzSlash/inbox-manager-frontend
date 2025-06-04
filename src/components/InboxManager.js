afd746b1e16b657d69c5dda4cfaac621c73e7027
FizzSlash <46427804+FizzSlash@users.noreply.github.com>
2025-06-04 01:56:54 -0500

    Update InboxManager.js

--- src/components/InboxManager.js
+++ src/components/InboxManager.js
@@ -1727,7 +1727,7 @@ const InboxManager = () => {
   const findPhoneNumber = async (lead) => {
     setIsSearchingPhone(true);
     try {
-      const response = await fetch('https://reidsickels.app.n8n.cloud/webhook-test/0b5749de-2324-45da-aa36-20971addef0b', {
+      const response = await fetch('https://reidsickels.app.n8n.cloud/webhook/0b5749de-2324-45da-aa36-20971addef0b', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
